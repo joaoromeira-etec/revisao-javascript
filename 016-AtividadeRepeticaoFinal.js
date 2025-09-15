dc447a67562cf32ef2f3f08b87c25d69fa5e7a53
joaoromeira-etec <joaopedrodiasromeira456@gmail.com>
2025-09-15 20:30:56 -0300

    aula 17 e 18 javascript
    aula de web javascript feita no dia 15 de setembro

--- 016-AtividadeRepeticaoFinal.js
+++ 016-AtividadeRepeticaoFinal.js
@@ -14,22 +14,25 @@ console.log(' ');
 let tabuadaFinal = entrada ('Agora, digite a última tabuada que quer ver: ');
 
 
+let contadorGeral = tabuadaInicial;
+
+
 
 // Erros Possíveis
   if (tabuadaInicial === '' || tabuadaFinal === '') {
-    erros.push(' - É necessário preencher todas as informações solicitadas!')
+    erros.push('É necessário preencher todas as informações solicitadas!')
  } else if (isNaN(tabuadaInicial) || isNaN(tabuadaFinal)) {
-    erros.push(' - As duas entradas não são números!')  
+    erros.push('As duas entradas não são números!')  
  } else {
     
     if (isNaN(tabuadaInicial)) {
-    console.log(' - O primeiro valor deve ser um número.');
+    console.log('O primeiro valor deve ser um número.');
 
  }
 
 
   if (isNaN(tabuadaFinal)) {
-    console.log(' - O segundo valor deve ser um número');
+    console.log('O segundo valor deve ser um número');
 
  }
  }
@@ -38,16 +41,20 @@ let tabuadaFinal = entrada ('Agora, digite a última tabuada que quer ver: ');
 
 
  // Lógica do Programa
+
  if (erros.length > 0) {
     console.log('\n Erros encontrados no programa: ');
-    erros.forEach(erro => console.log('- ' + erro))
- } else {
+    erros.forEach(erro => console.log('- ' + erro));
 
-    tabuadaInicial = parseInt(tabuadaInicial);
-    tabuadaFinal = parseInt(tabuadaFinal);
-    
-    for (let contador =1; contador <=10; contador++) {
-    console.log(`${tabuadaInicial} X ${contador} = ${parseInt(tabuadaInicial * contador)}`);
-    
-}
-}
+ } else while(contadorGeral <= tabuadaFinal) {
+         console.log(`TABUADA DO ${tabuadaInicial}`);
+         console.log('');
+
+         for (let cont = 1; cont <=10; cont++ ) {
+            console.log(`${tabuadaInicial} x ${cont} = ${parseInt(tabuadaInicial * cont)}`);
+         }
+
+         tabuadaInicial++;
+         contadorGeral++;
+         console.log('');
+ }
